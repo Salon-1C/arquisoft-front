a089871cf7e05fe71bdd29a7dbffc5c638bfd1e6
Juan Camilo <juroseros@unal.edu.co>
2026-04-05 12:53:30 -0500

    Add searchbar to /explore

--- src/components/classes/ClassGrid.tsx
+++ src/components/classes/ClassGrid.tsx
@@ -9,7 +9,9 @@ export default function ClassGrid({ classes }: ClassGridProps) {
   return (
     <div className="divide-y divide-border md:divide-y-0 md:space-y-3 md:px-20 md:py-5">
       {classes.map((cls) => (
-        <ClassCard key={cls.id} class={cls} />
+        <div key={cls.id} className="animate-card-appear">
+          <ClassCard class={cls} />
+        </div>
       ))}
     </div>
   )
