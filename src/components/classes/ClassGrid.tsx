9179d23ab37e8d46fe0683119e1ebe984e08532a
Juan Camilo <juroseros@unal.edu.co>
2026-04-05 12:45:41 -0500

    Create videos thumnails at /explore

--- src/components/classes/ClassGrid.tsx
+++ src/components/classes/ClassGrid.tsx
@@ -1,3 +1,16 @@
-export default function ClassGrid() {
-  return <div>ClassGrid</div>
+import type { Class } from '@/types/class'
+import ClassCard from './ClassCard'
+
+interface ClassGridProps {
+  classes: Class[]
+}
+
+export default function ClassGrid({ classes }: ClassGridProps) {
+  return (
+    <div className="divide-y divide-border md:divide-y-0 md:space-y-3 md:px-20 md:py-5">
+      {classes.map((cls) => (
+        <ClassCard key={cls.id} class={cls} />
+      ))}
+    </div>
+  )
 }
